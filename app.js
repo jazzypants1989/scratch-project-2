d2daeb9cac4807416a6ef298e70231e8079f4ca2
jazzypants1989 <jessepence@gmail.com>
2022-08-07 17:32:03 -0700

    Bing, bong, singing your song?

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-bong
+const menu = document.querySelector('#mobile-menu')
+const menuLinks = document.querySelector('.navbar_menu')
+
+// Display Mobile Menu
+
+const mobileMenu = () => {
+    menu.classList.toggle('is-active')
+    menuLinks.classList.toggle('active')
+}
+
+menu.addEventListener('click', mobileMenu);
